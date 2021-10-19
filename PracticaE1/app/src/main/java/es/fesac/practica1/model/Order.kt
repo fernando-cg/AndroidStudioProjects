6074d6f40ce5332f3fc2ea65f7e03032c220fa06
Fernando <fetmanditocamachito@gmail.com>
2021-10-19 14:28:12 +0200

    Cambios

--- PracticaE1/app/src/main/java/es/fesac/practica1/model/Order.kt
+++ PracticaE1/app/src/main/java/es/fesac/practica1/model/Order.kt
@@ -5,7 +5,7 @@ package es.fesac.practica1.model
  */
 //Creo las variables de esta clase
 class Order(var idO:Long,var devices:Map<Int,Int>,var dnic:String,var dniv:String,var prize:Double){
-    var points = if (prize%10==0.0) (prize/10).toInt() else (prize/10 +1).toInt() //Ejercicio 3 Propiedad que le da a la clase Order la cantidad de puntos que se le suman tras la compra
+    var points = if (prize%10==0.0) (prize/10).toInt() else (prize/10 +1).toInt() //Ejercicio 3 Propiedad que le da a la clase Order la cantidad de puntos que se le suman tras la compra buscar funcion de math para redondear
 }
 //Creo las variables de esta clase
 
--- PracticaE1/app/src/main/java/es/fesac/practica1/model/Seller.kt
+++ PracticaE1/app/src/main/java/es/fesac/practica1/model/Seller.kt
@@ -11,15 +11,16 @@ import java.util.*
  */
 class Seller(dni: String, name: String, surname: String, born: Date, var idS:Long) :Person(dni, name,
     surname, born), Sell{
+
     //Interfaz ejercicio 5
     override fun sell(devices: Map<Device, Int>, client: Client): Order {
-        var idCount = mutableMapOf<Int,Int>()
         var totalPrize = 0.0
+
         for(i in devices){
-            idCount[i.key.id] = i.value
             totalPrize += i.key.prize
         }
-        val order = Order((Math.random()*1000).toLong(),idCount,client.dni,this.dni,totalPrize)
+
+        val order = Order((Math.random()*1000).toLong(),devices.mapKeys { it.key.id },client.dni,this.dni,totalPrize)
         updateClientPoints(listOf(client), listOf(order))
         return order
     }
