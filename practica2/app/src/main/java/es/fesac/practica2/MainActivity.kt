91ff1b26b4347ae046df3c1c1b25b5589377e501
fernando <fetmanditocamachito@gmail.com>
2021-10-28 20:11:21 +0200

    Terminado

--- practica2/app/src/main/java/es/fesac/practica2/MainActivity.kt
+++ practica2/app/src/main/java/es/fesac/practica2/MainActivity.kt
@@ -6,12 +6,12 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
         // Descomentar para probar layout de login y comentando la línea 9
         //setContentView(R.layout.fragment_login)
 
         // Descomentar para probar layout de registro y comentando la línea 9
-        //setContentView(R.layout.fragment_login)
+        setContentView(R.layout.fragment_register)
     }
 }
