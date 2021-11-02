3d495eb9041fa1e1bbbf775d7451850836249a11
Fernando <fetmanditocamachito@gmail.com>
2021-11-02 14:23:06 +0100

    Ejercicioos

--- tictactoe/app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ tictactoe/app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onResume(){
+        super.onResume()
+        binding.multy.setOnClickListener{}
     }
 }
